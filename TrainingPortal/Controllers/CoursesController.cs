2f71352707056b3bcfe05f0599cca7d5b8826de4
sanjana0107 <sanjanasinghsg015@gmail.com>
2026-01-09 15:15:59 +0530

    day4

---
+++ TrainingPortal/Controllers/CoursesController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace TrainingPortal.Controllers
+{
+    public class CoursesController: Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
