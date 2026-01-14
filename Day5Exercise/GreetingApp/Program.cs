514f1cb381c5e17a252747f22acf57eb716d5fb0
sanjana0107 <sanjanasinghsg015@gmail.com>
2026-01-14 21:36:25 +0530

    Day5 Exercise

---
+++ Day5Exercise/GreetingApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using GreetingLibrary;
+
+namespace GreetingApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string? name=Console.ReadLine();
+            Console.WriteLine(GreetingHelper.GetGreeting(name));
+
+        }
+    }
+}
---
+++ Day5Exercise/GreetingLibrary/GreetingHelper.cs
@@ -0,0 +1,17 @@
+﻿namespace GreetingLibrary
+{
+    public class GreetingHelper
+    {
+        public static string GetGreeting(string? name)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                return "Hello, Guest!";
+            }
+            else
+                return $"Hello,{name}!";
+        }
+
+
+    }
+}
