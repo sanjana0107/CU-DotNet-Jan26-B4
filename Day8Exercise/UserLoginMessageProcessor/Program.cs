b9c4df56a8791721aeac39f0736bbbff7c903c69
sanjana0107 <sanjanasinghsg015@gmail.com>
2026-01-14 14:48:53 +0530

    Day8Exercise Solution

---
+++ Day8Exercise/UserLoginMessageProcessor/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace UserLoginMessageProcessor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter username and Login message");
+            string input=Console.ReadLine();
+            string[] inputs = input.Split('|', StringSplitOptions.TrimEntries);
+            string loginMessage=inputs[1].ToLower();
+            string status;
+            if (loginMessage.Contains("successful"))
+            {
+                status="LOGIN SUCCESS(CUSTOM MESSAGE)";            
+
+            }
+            else if(loginMessage.Equals("login successful"))
+            {
+                status = "LOGIN SUCCESS";
+            }
+            else
+            {
+                status = "LOGIN FAILED";
+            }
+            Console.WriteLine($"{"User"}{":",5}{inputs[0]}");
+            Console.WriteLine($"{"Message"}{":",2}{loginMessage}");
+            Console.WriteLine($"{"Status"}{":",3}{status}");
+
+
+
+            
+        }
+    }
+}
