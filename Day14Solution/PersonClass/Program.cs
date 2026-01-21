91044fa0335eb44a05939fb0de41fd14be739317
sanjana0107 <sanjanasinghsg015@gmail.com>
2026-01-21 15:09:54 +0530

    Day14 Soln

---
+++ Day14Solution/PersonClass/Program.cs
@@ -0,0 +1,65 @@
+﻿namespace PersonClass
+{
+    internal enum Departments
+    {
+        Accounts, IT, Sales
+    };
+    class Employee
+    {
+
+        int id;
+        public void SetId(int id)
+        {
+            this.id = id;
+        }
+        public int GetId()
+        {
+            return id;
+        }
+        public string Name { get; set; }
+
+
+        private Departments department;
+
+        public Departments Department
+        {
+            get { return department; }
+            set { department = value; }
+
+        }
+        private int salary;
+
+        public int Salary
+        {
+            get { return salary; }
+            set
+            {
+                if (value > 50000 && value < 90000)
+                    salary = value;
+            }
+        }
+
+        public void Display()
+        {
+            Console.WriteLine($"ID: {id}");
+            Console.WriteLine($"Name: {Name}");
+            Console.WriteLine($"Department: {department}");
+            Console.WriteLine($"Salary: {salary}");
+        }
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee e1 = new Employee();
+            e1.SetId(1);
+            e1.GetId();
+            e1.Name = "Shreya";
+            e1.Salary = 55000;
+            e1.Department = Departments.IT;
+            e1.Display();
+
+        }
+    }
+}
