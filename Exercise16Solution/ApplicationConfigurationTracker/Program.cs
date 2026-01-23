3932b40d6b29ea5a161ca724e5031c70a51160b4
sanjana0107 <sanjanasinghsg015@gmail.com>
2026-01-23 12:15:57 +0530

    Day16 Soln

---
+++ Exercise16Solution/ApplicationConfigurationTracker/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace ApplicationConfigurationTracker
+{
+    class ApplicationConfig
+    {
+        public static string ApplicationName { get; set; }
+
+        public static string Environment { get; set; }
+
+        public static int AccessCount { get; set; }
+
+        public static bool IsInitialized { get; set; }
+
+        static ApplicationConfig()
+        {
+            ApplicationName = "MyApp";
+            Environment = "Development";
+            AccessCount = 0;
+            IsInitialized = false;
+        }
+
+        public static void Initialize(string appName, string environment)
+        {
+            ApplicationName = appName;
+            Environment = environment;
+            IsInitialized = true;
+            AccessCount++;
+        }
+
+        public static string GetConfigurationSummary()
+        {
+            AccessCount++;
+            return $"Application Name- {ApplicationName}\nEnvironment- {Environment}\n" +
+                $"Access Count- {AccessCount}\nInitialization Status- {IsInitialized}";
+        }
+
+        public static void ResetConfiguration()
+        {
+            ApplicationName = "MyApp";
+            Environment = "Development";
+            IsInitialized = false;
+            AccessCount++;
+
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ApplicationConfig.ApplicationName = "NewApp";
+            ApplicationConfig.Initialize("WhatsApp", "Prod");
+            Console.WriteLine(ApplicationConfig.GetConfigurationSummary());
+            Console.WriteLine();
+            ApplicationConfig.ResetConfiguration();
+            Console.WriteLine(ApplicationConfig.GetConfigurationSummary());
+            
+
+
+        }
+    }
+}
