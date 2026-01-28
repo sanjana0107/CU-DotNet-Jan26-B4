2835a2b18660bd64d6fae2f51548ea4d8904f7f6
sanjana0107 <sanjanasinghsg015@gmail.com>
2026-01-28 12:04:47 +0530

    Day18 Soln

---
+++ Day18/Day18/Part01LoanEMISystem.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day18
+{
+    class Loan
+    {
+        public string LoanNumber { get; set; }
+
+        public string CustomerName { get; set; }
+
+        public decimal Amount { get; set; }
+
+        public int TenureYears { get; set; }
+
+        public Loan()
+        {
+            LoanNumber = string.Empty;
+            CustomerName = string.Empty;
+            Amount = 0;
+            TenureYears = 0;
+        }
+
+        public Loan(string loanNumber, string customerName, int amount, int tenureYears)
+        {
+            LoanNumber = loanNumber;
+            CustomerName = customerName;
+            Amount = amount;
+            TenureYears = tenureYears;
+        }
+
+        public double CalculateEMI()
+        {
+            double value = (double)Amount * TenureYears * 0.1;
+            return value;
+
+        }
+    }
+
+    class HomeLoan(string loanNumber, string customerName, int amount, int tenureYears) : Loan(loanNumber, customerName, amount, tenureYears)
+    {
+        public new double CalculateEMI()
+        {
+            double oneTimeValue = (double)Amount * 0.1;
+            double interest = (double)Amount * TenureYears * 0.08;
+            return oneTimeValue + interest;
+
+        }
+
+    }
+
+    class CarLoan : Loan
+    {
+        public CarLoan(string loanNumber, string customerName, int amount, int tenureYears) : base(loanNumber, customerName, amount, tenureYears)
+        { }
+        public new double CalculateEMI()
+        {
+
+            double interest = (double)Amount * TenureYears * 0.09;
+            return interest + 15000;
+
+        }
+
+    }
+
+    internal class Part01LoanEMISystem
+    {
+        static void Main(string[] args)
+        {
+            Loan[] loans = new Loan[4] {
+                new Loan("abc", "mkocds", 56098, 4),
+                new HomeLoan("sdbc", "cvcds", 1281170, 4),
+                new HomeLoan("xzcc", "asdcds", 5600991, 4),
+                new CarLoan("ewoid", "asdcds", 560231, 4)
+            };
+            for (int i = 0; i < loans.Length; i++)
+            {
+                Console.WriteLine(loans[i].CalculateEMI().ToString("N2"));
+            }
+
+        }
+    }
+}
---
+++ Day18/Day18/Part02EmployeeCompensationSystem.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day18
+{
+    class Employee
+    {
+        public int EmployeeId { get; set; }
+
+        public string EmployeeName { get; set; }
+
+        public decimal BasicSalary { get; set; }
+
+        public int ExperienceInYears { get; set; }
+
+        public decimal bonus;
+
+        public Employee(int id, string name, decimal salary, int years)
+        {
+            EmployeeId = id;
+            EmployeeName = name;
+            BasicSalary = salary;
+            ExperienceInYears = years;
+        }
+        public virtual decimal CalculateAnnualSalary()
+        {
+            return BasicSalary * 12;
+        }
+        public string DisplayEmployeeDetails()
+        {
+            return $"Employee Id-{EmployeeId}\nEmployee Name-{EmployeeName}" +
+                $"\nAnnual Salary-{CalculateAnnualSalary} ";
+        }
+    }
+
+    class PermanentEmployee : Employee
+    {
+        public PermanentEmployee(int id, string name, decimal salary, int years) : base(id, name, salary, years)
+        {
+
+
+        }
+        public override decimal CalculateAnnualSalary()
+        {
+            decimal hra = 0.20m * BasicSalary;
+            decimal specialAllowance = 0.10m * BasicSalary;
+            if (ExperienceInYears >= 5)
+            {
+                bonus = 50000;
+            }
+            return base.CalculateAnnualSalary() + hra + specialAllowance + bonus;
+        }
+    }
+
+    class ContractEmployee : Employee
+    {
+        public int ContractDurationInMonths { get; set; }
+        public ContractEmployee(int id, string name, decimal salary, int years) : base(id, name, salary, years)
+        {
+
+        }
+        public override decimal CalculateAnnualSalary()
+        {
+            if (ContractDurationInMonths >= 12)
+            {
+                bonus = 30000;
+            }
+            return base.CalculateAnnualSalary() + bonus;
+        }
+    }
+
+    class InternEmployee : Employee
+    {
+        public InternEmployee(int id, string name, decimal salary, int years) : base(id, name, salary, years)
+        {
+
+        }
+        public override decimal CalculateAnnualSalary()
+        {
+            return base.CalculateAnnualSalary();
+        }
+    }
+
+    internal class Part02EmployeeCompensationSystem
+    {
+        static void Main(string[] args)
+        {
+            Employee emp1 = new PermanentEmployee(1, "Sana", 76542, 6);
+            PermanentEmployee emp2 = new PermanentEmployee(2, "Megha", 23542, 3);
+            Console.WriteLine(emp1.CalculateAnnualSalary());
+            Console.WriteLine(emp2.CalculateAnnualSalary());
+
+        }
+
+    }
+}
