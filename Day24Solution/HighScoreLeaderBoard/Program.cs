3dc58f95a6420e615824af46f48ed29266844007
sanjana0107 <sanjanasinghsg015@gmail.com>
2026-02-03 15:06:21 +0530

    Day24 soln

---
+++ Day24Solution/HighScoreLeaderBoard/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace HighScoreLeaderBoard
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedDictionary<double,string> leaderboard = new SortedDictionary<double,string>();
+            leaderboard.Add(55.42, "SwiftRacer");
+            leaderboard.Add(52.10, "SpeedDemon");
+            leaderboard.Add(58.91, "SteadyEddie");
+            leaderboard.Add(51.05, "TurboTom");
+            foreach(var item in leaderboard)
+            {
+                Console.WriteLine($"Player Name: {item.Value, -12} Lap Time: {item.Key}");
+            }
+
+            Console.Write($"Gold medal time is {leaderboard.Keys.First()}");
+            double time = 0;
+            foreach (var item in leaderboard)
+            {
+                
+                if (item.Value == "SteadyEddie")
+                {
+                    time = item.Key;
+                }
+            }
+            leaderboard.Remove(time);
+            leaderboard.Add(54.00, "SteadyEddie");
+            
+
+        }
+    }
+}
---
+++ Day24Solution/LegacyEmployeeDirectory/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+
+namespace LegacyEmployeeDirectory
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Hashtable employeeTable= new Hashtable();
+            employeeTable.Add(101, "Alice");
+            employeeTable.Add(102, "Bob");
+            employeeTable.Add(103, "Charlie");
+            employeeTable.Add(104, "Diana");
+            if(!employeeTable.ContainsKey(105))
+                employeeTable.Add(105, "Edward");
+            else
+                Console.WriteLine("Key already exists. ");
+
+            string? employeeName = (string?) employeeTable[102];
+            Console.WriteLine($"Name of employee 102 is {employeeName}");
+
+            foreach (DictionaryEntry item in employeeTable)
+            {
+                Console.WriteLine($"ID: {item.Key}, Name: {item.Value}");
+            }         
+
+
+            employeeTable.Remove(103);
+            int count = 0;
+            foreach (DictionaryEntry item in employeeTable)
+                count++;
+            Console.WriteLine($"Total count of remaining employees: {count}");
+        }
+    }
+}
