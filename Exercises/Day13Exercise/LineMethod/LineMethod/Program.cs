31b5e274d96f3c975fd7295f42293cca7e2c30e4
sanjana0107 <sanjanasinghsg015@gmail.com>
2026-02-27 18:33:12 +0530

    updated

---
+++ Exercises/Day13Exercise/LineMethod/LineMethod/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace LineMethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            PrintPattern();
+            PrintPattern('+');
+            PrintPattern('$', 60);
+        }
+
+        static void PrintPattern(char ch = '-', int num = 40)
+        {
+            for (int i = 0; i < num; i++)
+            {
+                Console.Write(ch);
+            }
+            Console.WriteLine();
+        }
+    }
+}
