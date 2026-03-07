9e13e324e5c1be17e88be6aff015c22d4f83cbb4
sanjana0107 <sanjanasinghsg015@gmail.com>
2026-03-07 19:50:19 +0530

    Day 50 exercise

---
+++ Exercises/Day50Exercise/Day50Exercise/Program.cs
@@ -0,0 +1,63 @@
+﻿namespace Day50Exercise
+{
+    class Student
+    {
+        public int StudId { get; set; }
+
+        public string SName { get; set; }
+
+        public Student(int id, string name)
+        {
+            StudId = id;
+            SName = name;
+        }
+
+    }
+           
+    class ManageData
+    {
+        public Dictionary<Student, int> studentData = new Dictionary<Student, int>();
+        
+        public void AddStudent(Student stud, int marks)
+        {
+            if (!studentData.ContainsKey(stud))
+                studentData.Add(stud, marks);
+            else
+            {
+                if (studentData[stud] < marks)
+                    studentData[stud] = marks;
+            }
+        }
+        public void ShowData()
+        {
+            foreach (var item in studentData)
+            {
+                Console.WriteLine($"Name - {item.Key.SName} Marks - {item.Value}");
+            }
+        }
+
+    }
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Student s1 = new Student(1, "Aman");
+
+            ManageData md = new ManageData();
+            md.AddStudent(s1, 56);            
+           
+            md.AddStudent(new Student(2, "Riya"), 75);
+            
+            md.AddStudent(s1, 95);
+
+            md.ShowData();
+
+            
+
+            
+            
+            
+        }
+    }
+}
