b55bd1766902ea607a38a9c6750eb33972e14355
sanjana0107 <sanjanasinghsg015@gmail.com>
2026-03-17 21:04:33 +0530

    updated

--- Exercises/Day56Exercise/Day56Exercise/Controllers/LoanController.cs
+++ Exercises/Day56Exercise/Day56Exercise/Controllers/LoanController.cs
@@ -1,5 +1,6 @@
-﻿using Day56Exercise.Models;
-using Microsoft.AspNetCore.Http;
+﻿using System.Collections.Generic;
+using System.Linq;
+using Day56Exercise.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Day56Exercise.Controllers 
@@ -17,6 +18,7 @@ namespace Day56Exercise.Controllers
                 IsSettled = true
             }           
         };
+
         // GET: LoanController
         public ActionResult Index()
         {
@@ -27,6 +29,7 @@ namespace Day56Exercise.Controllers
         public ActionResult Details(int id)
         {
             var DetailOfLoanId = _loanDetails.FirstOrDefault(x => x.LoanId == id);
+            if (DetailOfLoanId == null) return NotFound();
             return View(DetailOfLoanId);
         }
 
@@ -41,22 +44,27 @@ namespace Day56Exercise.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Create(Loan loan)
         {
+            if (!ModelState.IsValid)
+                return View(loan);
+
             try
             {
+                // assign a new unique Id
+                loan.LoanId = _loanDetails.Any() ? _loanDetails.Max(l => l.LoanId) + 1 : 1;
                 _loanDetails.Add(loan);
                 return RedirectToAction(nameof(Index));
             }
             catch
             {
-                return View();
-            }
-            
+                return View(loan);
+            }  
         }
 
         // GET: LoanController/Edit/5
         public ActionResult Edit(int id)
         {
             var x = _loanDetails.FirstOrDefault(x => x.LoanId == id);
+            if (x == null) return NotFound();
             return View(x);
         }
 
@@ -65,21 +73,24 @@ namespace Day56Exercise.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Edit(int id, Loan loan)
         {
+            if (!ModelState.IsValid)
+                return View(loan);
+
             try
             {
                 var editedDetails = _loanDetails.FirstOrDefault(x => x.LoanId == id);
-                if(editedDetails != null)
-                {
-                    editedDetails.BorrowerName = loan.BorrowerName;
-                    editedDetails.LenderName =loan.LenderName;
-                    editedDetails.Amount = loan.Amount;
-                    editedDetails.IsSettled = loan.IsSettled;
-                }               
+                if (editedDetails == null) return NotFound();
+
+                editedDetails.BorrowerName = loan.BorrowerName;
+                editedDetails.LenderName = loan.LenderName;
+                editedDetails.Amount = loan.Amount;
+                editedDetails.IsSettled = loan.IsSettled;
+
                 return RedirectToAction(nameof(Index));
             }
             catch
             {
-                return View();
+                return View(loan);
             }            
         }
 
@@ -87,20 +98,21 @@ namespace Day56Exercise.Controllers
         public ActionResult Delete(int id)
         {
             var idDelete = _loanDetails.FirstOrDefault(x => x.LoanId == id);
+            if (idDelete == null) return NotFound();
             return View(idDelete);
         }
 
         // POST: LoanController/Delete/5
-        [HttpPost]
+        [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, Loan loan)
+        public ActionResult DeleteConfirmed(int id)
         {
             var idDelete = _loanDetails.FirstOrDefault(x => x.LoanId == id);
-            if(idDelete != null)
+            if (idDelete != null)
             {
                 _loanDetails.Remove(idDelete);
             }
-            return RedirectToAction("Index");
+            return RedirectToAction(nameof(Index));
         }
     }
 }
