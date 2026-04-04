f891bb2793352dbb805749bba5e68a65aa71f9f3
sanjana0107 <sanjanasinghsg015@gmail.com>
2026-04-04 23:04:33 +0530

    updated

--- WeeklyAssessments/Week13Assessment/TravelDestinationAPI/TravelDestination.MVC/Program.cs
+++ WeeklyAssessments/Week13Assessment/TravelDestinationAPI/TravelDestination.MVC/Program.cs
@@ -20,7 +20,7 @@ namespace TravelDestination.MVC
             if (!app.Environment.IsDevelopment())
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                
                 app.UseHsts();
             }
 
